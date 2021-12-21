06f0dac0de5f233d045ec8b308053c5f3be2f529
fernandobosco <fbosco@dlocal.com>
2021-12-21 11:44:52 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Deno CRUD API
+
+Simple REST Api made with deno and oak
+
+## How to run?
+
+if you have deno install, simple run:
+
+`deno run --allow-net index.ts`
---
+++ routes.ts
@@ -0,0 +1,13 @@
+import { Router } from "https://deno.land/x/oak/mod.ts";
+
+import { getBooks, getBook, addBook, updateBook, deleteBook } from './controller.ts'
+
+const router = new Router()
+
+router.get('/books', getBooks)
+    .get('/books/:isbn', getBooks)
+    .post('books', addBook)
+    .put('/books/:isbn', updateBook)
+    .delete('books/:isbn', deleteBook)
+
+export default router
